5a9f2dd5244c44506259d71c0702a4c79311244a
Fabio Nascimento <fabionascimento@smartbr.com>
2023-10-17 11:17:10 -0300

    feat: example de endpoints

--- src/main/java/br/com/arenapro/arenapro/PrimeiraController.java
+++ src/main/java/br/com/arenapro/arenapro/PrimeiraController.java
@@ -1,8 +1,9 @@
 package br.com.arenapro.arenapro;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.arenapro.arenapro.user.UserModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -11,5 +12,30 @@ public class PrimeiraController {
     @GetMapping("/session")
     public String primeiraController() {
         return "OLáaaaaaa";
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O para com metodo com query params é" + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O para com metodo com query params 2 é" + allParams.entrySet();
+    }
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComQueryParams2(@RequestBody UserModel user) {
+        return "O para com metodo com body params é" + user.getEmail();
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name) {
+        return "O metodo com headers" + name;
+    }
+
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+        return "O metodo com list de headers" + headers.entrySet();
     }
 }
